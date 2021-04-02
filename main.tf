4aa40bb6a77b65729cf1ffbc1925aeb66c9836ad
Thomas Gorham <thomasgorham@Thomass-MacBook-Pro.local>
2021-04-02 13:47:33 -0500

    Push initial config with empty COOS instance

---
+++ main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+variable "project_id" {
+  type = string
+}
+
+provider "google" {
+
+  credentials = file("credentials.json")
+
+  project = var.project_id
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_firewall" "http-ssh" {
+  name    = "allow-ssh"
+  network = "terraform-network"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
+
+resource "google_compute_firewall" "http-vault" {
+  name    = "allow-vault"
+  network = "terraform-network"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8200"]
+  }
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+        nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
